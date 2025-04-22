e54b71805a5af9fbf82b9b9ce1d9262bb40e6d60
Nama Anda <email@example.com>
2025-04-22 15:07:18 +0700

    buat migrate

---
+++ database/migrations/2025_04_22_080129_create_transaction_details_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('transaction_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transaction_id')->constrained()->onDelete('cascade');
+            $table->foreignId('service_id')->constrained()->onDelete('cascade');
+            $table->decimal('quantity', 8, 2); // dalam kg
+            $table->decimal('subtotal', 10, 2);
+            $table->timestamps();
+        });
+    }
+    
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction_details');
+    }
+};
